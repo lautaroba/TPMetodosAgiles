9d5b4665625268ab2063fcb5f52e2a5e5bf37bad
FerAleUtnSantaFe <fernandoale00@hotmail.com>
2024-05-25 20:01:11 -0300

    altatitularcasilisto

--- licencias2024/src/main/java/app/Controladores/AltaTitularController.java
+++ licencias2024/src/main/java/app/Controladores/AltaTitularController.java
@@ -2,23 +2,21 @@ package app.Controladores;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.RadioButton;
-import javafx.scene.control.SplitMenuButton;
 import javafx.scene.control.TextField;
 import javafx.scene.Node;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
 
-public class AltaTitularController implements Initializable{
+
+public class AltaTitularController  {
 
     @FXML
     private Button logoutButton;
@@ -28,76 +26,43 @@ public class AltaTitularController implements Initializable{
 
     @FXML
     private Button volverButton;
-
+    
     @FXML
-    private TextField nombreTextfield;
-
+    private TextField dniTextfield;
+    
     @FXML
     private TextField apellidoTextfield;
-
+    
     @FXML
     private TextField direccionTextfield;
-
+    
     @FXML
     private DatePicker fechaNacDatePicker;
-
+    
     @FXML
-    private SplitMenuButton claseSplitMenu;
-
+    private MenuButton claseMenu;
+    
     @FXML
-    private SplitMenuButton grupoSplitMenu;
-
+    private MenuButton grupoMenu;
+    
     @FXML
-    private SplitMenuButton RHSplitMenu;
-
+    private MenuButton RHMenu;
+    
+    @FXML
+    private MenuButton tipoMenu;
+    
     @FXML
     private RadioButton siRadioButton;
-
+    
     @FXML
     private RadioButton noRadioButton;
+    
+    @FXML
+    private TextField nombreTextfield;
     
     private Stage stage;
 	private Scene scene;
 	private Parent root;
-
-
-    @Override
-	public void initialize(URL location, ResourceBundle resources) {
-        cargarClaseLicencia();
-        cargarGrupoSanguineo();
-        cargarFactorRH();
-    }
-
-    private void cargarGrupoSanguineo(){
-        grupoSplitMenu.setText("Seleccione Grupo Sanguineo");
-        MenuItem grupoA = new MenuItem("Grupo A");
-        MenuItem grupoB = new MenuItem("Grupo B");
-        MenuItem grupoAB = new MenuItem("Grupo AB");
-        MenuItem grupoO= new MenuItem("Grupo O");
-        grupoSplitMenu.getItems().removeAll();
-        grupoSplitMenu.getItems().addAll(grupoA,grupoB,grupoAB,grupoO);
-    }
-
-    private void cargarFactorRH(){
-        RHSplitMenu.setText("Seleccione Factor RH");
-        MenuItem positivo = new MenuItem("Positivo");
-        MenuItem negativo = new MenuItem("Negativo");
-        RHSplitMenu.getItems().removeAll();
-        RHSplitMenu.getItems().addAll(positivo,negativo);
-    }
-
-    private void cargarClaseLicencia(){
-        claseSplitMenu.setText("Seleccione Clase de Licencia");
-        MenuItem claseA = new MenuItem("Clase A");
-        MenuItem claseB = new MenuItem("Clase B");
-        MenuItem claseC = new MenuItem("Clase C");
-        MenuItem claseD = new MenuItem("Clase D");
-        MenuItem claseE = new MenuItem("Clase E");
-        MenuItem claseF = new MenuItem("Clase F");
-        MenuItem claseG = new MenuItem("Clase G");
-        claseSplitMenu.getItems().removeAll();
-        claseSplitMenu.getItems().addAll(claseA, claseB, claseC, claseD, claseE, claseF, claseG);
-    }
 
     @FXML
     private void logout(ActionEvent event) {
@@ -120,5 +85,11 @@ public class AltaTitularController implements Initializable{
 
     }
 
+    @FXML
+    private void handleMenuSelection(ActionEvent event) {
+        MenuItem selectedItem = (MenuItem) event.getSource();
+        MenuButton parentButton = (MenuButton) selectedItem.getParentPopup().getOwnerNode();
+        parentButton.setText(selectedItem.getText());
+    }
     
 }
